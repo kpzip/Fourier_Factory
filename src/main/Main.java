5e0a621e806f66ba82d994ba1adfb6a4bd643f0a
kpzip <kpzipgames@gmail.com>
2021-04-22 20:24:19 -0700

    4/22/2021

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,10 @@
 package main;
 
+//Main class with the main method
 public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 
 }
--- src/main/math/MathHelper.java
+++ src/main/math/MathHelper.java
@@ -1,5 +1,6 @@
 package main.math;
 
+//class meant for stray functions that help with the math in some way
 public class MathHelper {
 
 }
