e97bac9f96ab90501ca0ff17b219372c1fe7c2fb
Yegor Tryndin <yegortryndin@gmail.com>
2021-04-23 08:24:11 -0700

    Add renderer package and window class

--- src/com/kpzip/fourier/Main.java
+++ src/com/kpzip/fourier/Main.java
@@ -4,7 +4,7 @@ package com.kpzip.fourier;
 public class Main {
 	
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		
 	}
 
 }
---
+++ src/com/kpzip/fourier/renderer/Window.java
@@ -0,0 +1,7 @@
+package com.kpzip.fourier.renderer;
+
+
+
+public class Window {
+
+}
